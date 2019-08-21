4e87ab8d3c2943e8269319edf5941de184ab900e
Alexander Fröber <alex.froeber@gmail.com>
2019-08-21 09:10:09 +0200

    Fixed github url

--- modules/utility.py
+++ modules/utility.py
@@ -178,7 +178,7 @@ class UtilityModule(cmd.Cog):
     @cmd.command(name="source", aliases=["sourcecode", "github", "git"])
     async def source(self, ctx):
         await ctx.send(
-            "How nice that you want to contribute! <https://github.com/froebera/notbots>"
+            "How nice that you want to contribute! <https://github.com/froebera/notbot>"
         )
 
     @cmd.command(hidden=True)
