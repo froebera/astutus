358729dbfeafc7f365ca1a40f1dbdcdefacae113
froebera <alex.froeber@gmail.com>
2019-08-24 22:52:04 +0200

    Removed console logs

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -4,12 +4,8 @@ from discord.ext import commands
 
 class Queue(commands.Converter):
     async def convert(self, ctx, argument):
-        print(f"converter {argument}")
-        queues = await get_redis_connection().lrange(
-            f"raid:{ctx.guild.id}:queues"
-        )
+        queues = await get_redis_connection().lrange(f"raid:{ctx.guild.id}:queues")
         if not argument in queues:
-            print("raising exception")
             raise commands.BadArgument(
                 f"Queue **{argument}** does not exist. Available queues: {', '.join(queues)}"
             )
