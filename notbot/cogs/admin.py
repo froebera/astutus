a17c389fbb91a0a6a093a32bc6863a52c20cc0e6
froebera <alex.froeber@gmail.com>
2020-03-14 23:53:31 +0100

    stuff

--- notbot/cogs/admin.py
+++ notbot/cogs/admin.py
@@ -68,7 +68,7 @@ class AdminModule(commands.Cog, Module):
     @admin.command(name="rc")
     async def admin_create_role(self, ctx):
         author = ctx.message.author
-        await ctx.guild.create_role(name="role name")
+        await ctx.guild.create_role(name="role name", colour=0x3cd2ed)
 
 
 def setup(bot):
